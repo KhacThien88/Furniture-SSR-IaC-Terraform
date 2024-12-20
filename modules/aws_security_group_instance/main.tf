1d043247866d2c7fc33c7e903999e08c20280dac
KhacThien88 <KTeightop1512@gmail.com>
2024-12-20 18:45:16 +0700

    update sg for instance

--- modules/aws_security_group_instance/main.tf
+++ modules/aws_security_group_instance/main.tf
@@ -3,6 +3,7 @@ resource "aws_security_group" "jenkins-sg" {
   description = "Allow TCP/8080 & TCP/22"
   vpc_id      = var.vpc_id
 
+  # Quy tắc cho SSH (cổng 22)
   ingress {
     description = "Allow 22 from our public IP"
     from_port   = 22
@@ -11,17 +12,32 @@ resource "aws_security_group" "jenkins-sg" {
     cidr_blocks = [var.external_ip]
   }
 
+
   ingress {
-    description = "Allow traffic from us-west-1"
+    description = "Allow anyone on port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow traffic from subnet"
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = [var.subnet_1]
   }
 
+  # Quy tắc outbound (egress)
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
+  tags = {
+    Name = "jenkins-sg"
+  }
+}

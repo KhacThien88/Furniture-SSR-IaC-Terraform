d953c0c77ff2fc8342997048996aeb5b35d99786
KhacThien88 <KTeightop1512@gmail.com>
2024-12-21 18:41:33 +0700

    update jenkinsfile

--- modules/aws_instance/main.tf
+++ modules/aws_instance/main.tf
@@ -6,24 +6,24 @@ resource "aws_instance" "jenkins-master" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [var.sg_id]
   subnet_id                   = var.subnet_id_1
-#   provisioner "local-exec" {
-#     command = <<EOF
-# aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region} --instance-ids ${self.id} 
-# ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ${var.ansible_playbook_path}
-# EOF
-#   }
-user_data = base64encode(<<-EOT
-#!/bin/bash
-echo "ubuntu:111111aA@" | chpasswd
-sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_config
-sed -i 's/^#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
-systemctl restart ssh 
-systemctl restart sshd 
-systemctl restart sshd.service 
-EOT
-)
+  provisioner "local-exec" {
+    command = <<EOF
+aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region} --instance-ids ${self.id} 
+ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ${var.ansible_playbook_path}
+EOF
+  }
+# user_data = base64encode(<<-EOT
+# #!/bin/bash
+# echo "ubuntu:111111aA@" | chpasswd
+# sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+# sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_config
+# sed -i 's/^#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+# systemctl restart ssh 
+# systemctl restart sshd 
+# systemctl restart sshd.service 
+# EOT
+# )
 
   tags = {
     Name = var.tag

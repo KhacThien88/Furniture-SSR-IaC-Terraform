104a7fefef5bead832e87e37730fe8a08ca1c2ce
KhacThien88 <KTeightop1512@gmail.com>
2024-12-21 23:55:07 +0700

    update instances

--- modules/aws_instance/main.tf
+++ modules/aws_instance/main.tf
@@ -6,6 +6,11 @@ resource "aws_instance" "jenkins-master" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [var.sg_id]
   subnet_id                   = var.subnet_id_1
+  root_block_device {
+    volume_size           = 30
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
   provisioner "local-exec" {
     command = <<EOF
 aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region} --instance-ids ${self.id} 

eebc20ba616120d0441fc37d50caa042375c4a98
KhacThien88 <KTeightop1512@gmail.com>
2025-01-13 12:01:52 +0700

    update

--- modules/aws_security_group_instance/main.tf
+++ modules/aws_security_group_instance/main.tf
@@ -243,6 +243,13 @@ resource "aws_security_group" "jenkins-sg" {
     cidr_blocks = [var.external_ip]
   }
   egress {
+    description = "Allow 9100 from our public IP"
+    from_port   = 8000
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = [var.external_ip]
+  }
+  egress {
     description = "Allow 9200 from our public IP"
     from_port   = 9200
     to_port     = 9200
